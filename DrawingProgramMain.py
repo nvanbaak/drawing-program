a4b4c1463c163e531d5c64ef1e03e3d5184ed650
Shine May Hung <shinemay@hotmail.com>
2021-11-21 18:27:45 -0800

    Modified DrawingProgramMain.py by SM

--- DrawingProgramMain.py
+++ DrawingProgramMain.py
@@ -2,6 +2,7 @@
 # Sorts the shapes, adds some more ahpes, replaces some shapes, sorts again.
 
 from drawing_program import DrawingProgram
+from shapefactory import ShapeFactory
 from square import Square
 from circle import Circle
 from triangle import Triangle
@@ -10,36 +11,47 @@ from rectangle import Rectangle
 print("Create a class called DrawingProgramMain that creates a DrawingProgram.")
 my_drawing_program = DrawingProgram()
 print("Add shapes to it: ")
-my_square = Square()
-my_square.set_side(10)
-my_circle = Circle()
-my_circle.set_radius(1)
-another_circle = Circle()
-another_circle.set_radius(2)
+
+my_rectangle = ShapeFactory.create_shape("rectangle", 2, 4.0)
+
+my_square = ShapeFactory.create_shape("square", 3.1)
+
+my_triangle = ShapeFactory.create_shape("triangle", 3, 4.0)
+
+my_circle = ShapeFactory.create_shape("circle", 3.1)
+
 my_drawing_program.addShape(my_square)
 my_drawing_program.addShape(my_circle)
-my_drawing_program.addShape(another_circle)
+my_drawing_program.addShape(my_rectangle)
+my_drawing_program.addShape(my_triangle)
 print(my_drawing_program)
-print("Sort the shapes: ")
+
+print("Sort the shapes by area: ")
 my_drawing_program.sort_shapes()
 print(my_drawing_program)
 
 print("Add more shapes: ")
-my_triangle = Triangle()
-my_triangle.set_height(3)
-my_triangle.set_base(4)
-my_rectangle = Rectangle()
-my_rectangle.set_width(2)
-my_rectangle.set_length(4)
+
+my_rectangle = ShapeFactory.create_shape("rectangle", 1, 2)
+
+my_square = ShapeFactory.create_shape("square", 2)
+
+my_triangle = ShapeFactory.create_shape("triangle", 1, 2)
+
+my_circle = ShapeFactory.create_shape("circle", 4)
+
 my_drawing_program.addShape(my_triangle)
 my_drawing_program.addShape(my_rectangle)
+my_drawing_program.addShape(my_square)
+my_drawing_program.addShape(my_circle)
 print(my_drawing_program)
 
 print("Replace some shapes: ")
 my_drawing_program.remove_shape(my_circle)
+another_circle = ShapeFactory.create_shape("circle", 4)
 my_drawing_program.addShape(another_circle)
 print(my_drawing_program)
 
-print("Sort shapes again: ")
+print("Sort shapes again by area: ")
 my_drawing_program.sort_shapes()
 print(my_drawing_program)
